49dbb8ec730b00a4e08560183de190ec57976661
kartikgoel1999 <52077666+kartikgoel1999@users.noreply.github.com>
2020-01-27 22:41:22 +0530

    Update chalk.js

--- chalk.js
+++ chalk.js
@@ -4,4 +4,4 @@ const figlet=require("figlet");
 console.log(chalk.cyan('I am cyan text'));
 console.log(chalk.blue('I am blue text'));
 console.log(chalk.red('I am red text'));
-console.log(chalk.green(figlet.textSync('Tasker')));
+console.log(chalk.green(figlet.textSync('Hello World!!')));
